393ab8eb7f20f6c3fd443b69d5ed82eb5d354d2c
FilinDmitry <filin.dmitry07@gmail.com>
2026-05-19 22:25:24 +0300

    страница профиля

--- UP01/Pages/ProfilePage.xaml.cs
+++ UP01/Pages/ProfilePage.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using UP01.Models;
+using UP01.Windows;
 
 namespace UP01.Pages
 {
@@ -22,7 +24,20 @@ namespace UP01.Pages
     {
         public ProfilePage()
         {
+            this.DataContext = Core.Context.Users.First();
             InitializeComponent();
+            LB_comments.ItemsSource = Core.Context.Users.First().Reviews.ToList();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new FreezePage());
+        }
+
+        private void Became_author_click(object sender, RoutedEventArgs e)
+        {
+            ApplicationWindow window = new ApplicationWindow();
+            window.Show();
         }
     }
 }
