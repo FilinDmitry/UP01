9f003fa704697ac9eea5f184ceafc7b0792ecfdb
FilinDmitry <filin.dmitry07@gmail.com>
2026-05-20 00:01:46 +0300

    ViewModel для ReadingList

--- UP01/Pages/BookListPage.xaml.cs
+++ UP01/Pages/BookListPage.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using UP01.Models;
+using UP01.ViewModels;
 
 namespace UP01.Pages
 {
@@ -20,9 +22,67 @@ namespace UP01.Pages
     /// </summary>
     public partial class BookListPage : Page
     {
+        ListBox LB_ReadingList;
+        List<BookInListViewModel> lst_book;
+        List<BookInListViewModel> lst_book_readed;
+        List<BookInListViewModel> lst_book_planed;
+        List<BookInListViewModel> lst_book_reading;
+        List<BookInListViewModel> lst_book_dust;
         public BookListPage()
         {
+            
             InitializeComponent();
+            lst_book = Core.Context.Books.Select(
+                b => new BookInListViewModel()
+                {
+                    book = b
+                }
+                ).ToList();
+           
         }
+
+        private void TabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            TabControl tab = sender as TabControl;
+            TabItem item = tab.SelectedItem as TabItem;
+            switch (item.Tag.ToString())
+            {
+                case ("Все"):
+                    break;
+                case ("Прочитано"):
+                    break;
+                case ("Читаю"):
+                    break;
+                case ("В планах"):
+                    break;
+                case ("Заброшено"):
+                    break;
+            }
+        }
+        void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            ListBox lbx = FindVisualChildByName<ListBox>(this.ListTabControl, "LB_ReadingList");
+            lbx.ItemsSource = lst_book;
+        }
+
+        private T FindVisualChildByName<T>(DependencyObject parent, string name) where T : FrameworkElement
+        {
+            T child = default(T);
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(parent); i++)
+            {
+                var ch = VisualTreeHelper.GetChild(parent, i);
+                child = ch as T;
+                if (child != null && child.Name == name)
+                    break;
+                else
+                    child = FindVisualChildByName<T>(ch, name);
+
+                if (child != null) break;
+            }
+            return child;
+        }
+
+        // Использование
+
     }
 }
---
+++ UP01/ViewModels/BookInListViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UP01.Models;
+
+namespace UP01.ViewModels
+{
+    public class BookInListViewModel : BookViewModel
+    {
+        public string Status => GetStatus();
+        public string GetStatus()
+        {
+            ReadingList reading = Core.Context.ReadingList.FirstOrDefault(rl => rl.UserID == Auth.cur_user.ID && rl.BookID == book.ID);
+            if (reading == null)
+            {
+                return string.Empty;
+            }
+            return reading.BookStatus.Name;
+        }
+    }
+}
